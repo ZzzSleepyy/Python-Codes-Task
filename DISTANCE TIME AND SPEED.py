d1f0a4a8a465eb49a44e604066ed52027a3b9e32
Niqouu <100042636+ZzzSleepyy@users.noreply.github.com>
2023-09-15 20:33:57 +0800

    DISTANCE TIME AND SPEED.py

---
+++ DISTANCE TIME AND SPEED.py
@@ -0,0 +1,7 @@
+#CALCULATE DISTANCE TIME TO SPEED
+distance = int(input("Distance(meters): "))
+time = int(input("Time(seconds): "))
+
+calculate = float(distance / time)
+
+print(f"Speed: {calculate} mph")
