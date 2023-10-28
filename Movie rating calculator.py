96e3f3920c29845bdce5a2c14e21e6a5c3865f7e
Niqouu <100042636+ZzzSleepyy@users.noreply.github.com>
2023-10-28 18:28:01 +0800

    Movie rating calculator.py

--- Movie rating calculator.py
+++ Movie rating calculator.py
@@ -1,16 +1,39 @@
-
+import os
 movie_ratings = {}
-
+ratings = []
 while True:
-    title = input("Enter a movie title (or 'q' to quit): ")
-    
-    if title.lower() == 'q':
-        break
-    
-    rating = float(input("Enter a rating (1-5): "))
-    
-    if title in movie_ratings:
-        movie_ratings[title] = (rating)
-        print(movie_ratings)
-    else:
-        movie_ratings[title] = rating
+    try:
+        title = input("Enter a movie title (or 'q' to quit): ")
+        
+        
+        if title.lower() == 'q':
+            break
+        
+        rating = float(input("Enter a rating (1-5): "))
+        f = title.split()
+        if title:
+            movie_ratings[title] = rating
+            ratings.append(rating)
+            for i, f in enumerate(movie_ratings, start=0):
+                i += 1
+                print(f"{i}. {f}: {rating}")
+        
+        elif rating > 5:
+            print("Please enter: 1-5")
+        
+
+        
+    except ValueError:
+        print("Value error: Try again")
+        
+
+# Calculate average ratings for each movie
+
+    if rating == 5:
+        pass
+
+# Find the highest-rated and lowest-rated movies
+
+# Create a menu for users to choose various operations, such as searching, listing, etc.
+
+# Implement the selected operation and display the results
